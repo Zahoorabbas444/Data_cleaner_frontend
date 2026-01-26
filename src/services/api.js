8bd4403af425fd424810a62a21d3b8a5a564aa1a
Hassan Mehdi <zahoorabbas444@gmail.com>
2026-01-27 01:17:50 +0500

    remove header

--- src/services/api.js
+++ src/services/api.js
@@ -26,9 +26,6 @@ export const uploadFile = async (file) => {
   formData.append('file', file);
 
   const response = await api.post('/upload', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
   });
   return response.data;
 };
