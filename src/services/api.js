044c797e9ab51f503614c3025738b620cd747709
Hassan Mehdi <zahoorabbas444@gmail.com>
2026-02-13 01:54:47 +0500

    add api in js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://data-cleaner-backend-production.up.railway.app';
+const API_BASE_URL = 'https://data-cleaner-backend-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
