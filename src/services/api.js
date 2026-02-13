6fe86c4185f257a60caec597726b12ec3480bdd3
Hassan Mehdi <zahoorabbas444@gmail.com>
2026-02-13 10:50:44 +0500

    add vercel frontend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://data-cleaner-backend-production.up.railway.app/api';
+const API_BASE_URL = 'https://data-cleaner-frontend-ten.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
